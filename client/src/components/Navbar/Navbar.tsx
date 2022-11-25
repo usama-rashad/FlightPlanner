c7d9a4eca58711fd571173a902353dde20757f82
Usama Rashad <usamakr@gmail.com>
2022-11-25 12:40:46 +0500

    Initial commit

---
+++ client/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Navbar.scss";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <span className="title">Trip Planner</span>
+    </div>
+  );
+}
+
+export default Navbar;
