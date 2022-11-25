c0523a29bc5b7c489832a743c7ec58e079d0a2b5
Usama Rashad <usamakr@gmail.com>
2022-11-25 13:36:29 +0500

    Update
    Update

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 import "./Navbar.scss";
 
+// Icons
+import MenuClosedIcon from "@mui/icons-material/Menu";
+
+// Buttons
+import Button from "@mui/material/Button";
+
 function Navbar() {
   return (
     <div className="navbar">
+      <div className="navbarButtons">
+        <MenuClosedIcon className="navbarIcons" />
+        <Button variant="contained">Help</Button>
+      </div>
+
       <span className="title">Trip Planner</span>
     </div>
   );
