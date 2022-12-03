84e9c97cc7f4cae01cb77ef7bf070e0a917c5074
Usama Rashad <usamakr@gmail.com>
2022-12-03 14:01:21 +0500

    Update

--- client/src/components/Searchbar/Searchbar.tsx
+++ client/src/components/Searchbar/Searchbar.tsx
@@ -15,6 +15,7 @@ import { MobileDatePicker } from "@mui/x-date-pickers/MobileDatePicker";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { Checkbox } from "@mui/material";
+import red from "@mui/material/colors/red";
 
 function Searchbar() {
   const [departureDate, setDepartureDate] = useState<Dayjs | null>(null);
@@ -38,50 +39,52 @@ function Searchbar() {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <div className="searchbar">
-        <div className="searchOptions">
-          <label>Return</label>
-          <input type="radio" name="flightType" />
-          <label>One way</label>
-          <input type="radio" name="flightType" />
-          <label>Multi-city</label>
-          <input type="radio" name="flightType" />
-        </div>
-        <div className="searchParameters">
-          <div className="searchData">
-            <input type="text" placeholder="Departure" className="city" />
-            <input type="text" placeholder="Arrival" className="city" />
-            <DesktopDatePicker
-              className="date"
-              label="Departure"
-              inputFormat="MM/DD/YYYY"
-              onChange={(newDate) => setDepartureDateAction(newDate)}
-              value={departureDate}
-              renderInput={(params: any) => (
-                <TextField size="small" variant="outlined" {...params} />
-              )}
-            />
-            <DesktopDatePicker
-              className="date"
-              label="Return"
-              inputFormat="MM/DD/YYYY"
-              onChange={(newDate) => setReturnDateAction(newDate)}
-              value={returnDate}
-              renderInput={(params: any) => (
-                <TextField size="small" variant="outlined" {...params} />
-              )}
-            />
-          </div>
-        </div>
-        <div className="searchSection">
+        <div className="searchBarContainer">
           <div className="searchOptions">
-            <label>
-              Include Star <StarAlliance className="icon" /> Alliance airlines
-            </label>
-            <Checkbox color="error" />
+            <label>Return</label>
+            <input type="radio" name="flightType" />
+            <label>One way</label>
+            <input type="radio" name="flightType" />
+            <label>Multi-city</label>
+            <input type="radio" name="flightType" />
           </div>
-          <Button variant="contained" size="small" className="searchButton">
-            Search
-          </Button>
+          <div className="searchParameters">
+            <div className="searchData">
+              <input type="text" placeholder="Departure" className="city" />
+              <input type="text" placeholder="Arrival" className="city" />
+              <DesktopDatePicker
+                className="date"
+                label="Departure"
+                inputFormat="MM/DD/YYYY"
+                onChange={(newDate) => setDepartureDateAction(newDate)}
+                value={departureDate}
+                renderInput={(params: any) => (
+                  <TextField size="small" variant="outlined" {...params} />
+                )}
+              />
+              <DesktopDatePicker
+                className="date"
+                label="Return"
+                inputFormat="MM/DD/YYYY"
+                onChange={(newDate) => setReturnDateAction(newDate)}
+                value={returnDate}
+                renderInput={(params: any) => (
+                  <TextField size="small" variant="outlined" {...params} />
+                )}
+              />
+            </div>
+          </div>
+          <div className="searchSection">
+            <div className="searchOptions">
+              <label>
+                Include Star <StarAlliance className="icon" /> Alliance airlines
+              </label>
+              <Checkbox color="error" />
+            </div>
+            <Button variant="contained" size="small" className="searchButton">
+              Search
+            </Button>
+          </div>
         </div>
       </div>
     </LocalizationProvider>
