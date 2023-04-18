3c5b80886afb674b4bb314c0364929984e8f6592
usama rashad <admin@sssoftware.one>
2023-04-18 14:00:25 +0500

    Update NavbarWithProvider.tsx

--- client/src/components/Common/Navbar/NavbarWithProvider.tsx
+++ client/src/components/Common/Navbar/NavbarWithProvider.tsx
@@ -1,1 +1,12 @@
 import Navbar from "../Navbar/Navbar";
+
+
+import React from 'react'
+
+function NavbarWithProvider() {
+  return (
+    <div>NavbarWithProvider</div>
+  )
+}
+
+export default NavbarWithProvider
